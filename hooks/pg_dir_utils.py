13478ba94cc9b511a0a0109047831809f60cce19
Junaid Ali <junaidali@plumgrid.com>
2016-10-27 21:47:56 +0500

    Minor improvements
    Signed-off-by: Junaid Ali <junaidali@plumgrid.com>

--- hooks/pg_dir_utils.py
+++ hooks/pg_dir_utils.py
@@ -686,7 +686,6 @@ def get_cidr_from_iface(interface):
 
 
 def director_cluster_ready():
-    print pg_dir_context._pg_dir_ips()
     dirs_count = len(pg_dir_context._pg_dir_ips())
     return True if dirs_count == 2 else False
 
